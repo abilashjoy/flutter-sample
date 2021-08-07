f166e481233384484f6d4725f7ee690208030dc6
Abilash Joy <abilashjoy13@gmail.com>
2021-08-07 12:29:16 +0530

    first trail

---
+++ sampleproject/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('my first app '),
+        centerTitle: true,
+        backgroundColor: Colors.red[400],
+      ),
+      body: Center(
+        child: RaisedButton.icon(
+          onPressed: () {},
+          icon: Icon(Icons.mail),
+          label: Text('mail me'),
+          color: Colors.amber,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+        backgroundColor: Colors.red[400],
+        onPressed: () {},
+      ),
+    );
+  }
+}
